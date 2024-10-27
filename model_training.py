2277684938d66251678c5674751d0653afda57d0
Edu-B <edu60009@gmail.com>
2024-10-27 17:22:39 -0400

    chore: Refactor model_training.py and process_data.py
    - Refactor the training function in model_training.py to accept X and y as parameters
    - Refactor the load_data function in process_data.py to accept csv_path as a parameter and return X, y, and dict_of_cast

--- model_training.py
+++ model_training.py
@@ -4,14 +4,12 @@ import tensorflow as tf
 from sklearn.linear_model import LinearRegression
 
 
-def training():
-    X,y = load_data()
+def training(X, y):
 
-    # if len(X.shape) == 2:
-    #     X = X.reshape((X.shape[0],1,X.shape[1]))
+    X_train, X_test, Y_train, Y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42
+    )
 
-    X_train, X_test, Y_train, Y_test = train_test_split(X,y,test_size=0.2, random_state=42)
-        
     model = LinearRegression()
     trained_model = model.fit(X_train, Y_train)
 
